88dcb0bfb5bf59d922c4db0725875d097a0fad70
kevinpagtakhan <kevpagtakhan@gmail.com>
2017-01-06 21:35:42 -0500

    Test cases for POST request on /posts route

--- test/posts.js
+++ test/posts.js
@@ -30,4 +30,64 @@ describe('Posts', () => {
         });
     });
   });
+
+  describe('POST /posts', () => {
+    it('should not post without a title', (done) => {
+      let post = {
+        author: 'Kevin'
+      }
+
+      chai.request(server)
+        .post('/posts')
+        .send(post)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('errors');
+          res.body.errors.should.have.property('title');
+          res.body.errors.title.should.have.property('kind').eql('required');
+
+          done();
+      });
+    });
+
+    it('should not post without an author', (done) => {
+      let post = {
+        title: 'Hello World!'
+      }
+
+      chai.request(server)
+        .post('/posts')
+        .send(post)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('errors');
+          res.body.errors.should.have.property('author');
+          res.body.errors.author.should.have.property('kind').eql('required');
+
+          done();
+      });
+    });
+
+    it('should post a book', (done) => {
+      let post = {
+        title: 'Hello World!',
+        author: 'Kevin'
+      }
+
+      chai.request(server)
+        .post('/posts')
+        .send(post)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.a.property('post');
+          res.body.post.should.have.a.property('title');
+          res.body.post.should.have.a.property('author');
+
+          done();
+      });
+    });
+  });
 });
